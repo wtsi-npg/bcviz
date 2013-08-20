08e948dc41ab9f28ca9c89582d0bd7d4e2d0f72f
DavidBryson <db15@sanger.ac.uk>
2013-08-20 16:52:57 +0100

    removed duplicate files

--- js/divSelections.js
+++
@@ -1,7 +0,0 @@
-function checkDivSelection (divID) {
-    if(!isNaN(parseInt(divID)) || !/\S/.test(divID) || d3.select(divID).empty()) {
-        return "body";
-    }else{
-        return divID;
-    }
-}
