839caf1c4d9ce3b5e70da99413fea5843cdb9b33
jmtcsngr <jmtc@sanger.ac.uk>
2016-05-03 14:50:00 +0100

    Tests compatible w/grunt-contrib-qunit@1.2.0

--- test/main.js
+++ test/main.js
@@ -1,23 +1,25 @@
 
 "use strict";
-require.config({
+requirejs.config({
   baseUrl: '../',
   paths: {
-    jquery: 'external/jquery/dist/jquery',
-    d3:     'external/d3/d3'
+    'qunit': 'external/qunit/qunit/qunit',
+    jquery:  'external/jquery/dist/jquery',
+    d3:      'external/d3/d3'
   },
   shim: {
     d3: { exports: 'd3' }
   }
 });
 
-require(['test/qcjson/insertSizeHistogram','test/qcjson/adapter', 'test/qcjson/mismatch'],  
+requirejs(['test/qcjson/insertSizeHistogram','test/qcjson/adapter', 'test/qcjson/mismatch'],  
   function(insertSizeHistogram, adapter, mismatch) {
+    // start QUnit because it was told to wait in the html.
+    QUnit.config.autostart = false;
     // load the tests.
     adapter.run();
     insertSizeHistogram.run();
     mismatch.run();
-    // run the tests.
     QUnit.start();
   }
 );
